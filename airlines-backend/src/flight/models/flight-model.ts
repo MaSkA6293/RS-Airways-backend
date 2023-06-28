7c4a57e0e773bb56c59866fb1b1d8fcf0047ed2a
Oleg Shkalikov <maska6293@yandex.ru>
2023-06-28 13:36:20 +0300

    refactor: separate flight model and entity

--- airlines-backend/src/flight/models/flight-model.ts
+++ airlines-backend/src/flight/models/flight-model.ts
@@ -1,23 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { AirportEntity } from 'src/airport/entities/airport.entity';
+import { Column } from 'typeorm';
 import { FlightPriceList } from './flight-price-list.model';
 
-@Entity('flight')
 export class FlightModel {
   @ApiProperty({
     example: '1361f75f-9092-41a1-8bc7-bbd7aec51da6',
     description: 'Unique id',
   })
-  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @ApiProperty({
     example: '2024-02-20T16:30:00.000Z',
     description: 'TakeOff date and time',
-  })
-  @Column({
-    nullable: false,
   })
   takeOffDate: Date;
 
@@ -25,17 +19,11 @@ export class FlightModel {
     example: '2024-02-20T20:15:00.000Z',
     description: 'Landing date and time',
   })
-  @Column({
-    nullable: false,
-  })
   landingDate: Date;
 
   @ApiProperty({
     example: 225,
     description: 'All flight in minutes',
-  })
-  @Column({
-    nullable: false,
   })
   flightDuration: number;
 
@@ -49,24 +37,8 @@ export class FlightModel {
   seatsAvailable: number;
 
   @ApiProperty({ example: 10, description: 'The quantity of the free seats' })
-  @Column({
-    nullable: false,
-  })
   seatsTotal: number;
 
   @ApiProperty({ example: 150, description: 'The trip price' })
-  @Column({
-    nullable: false,
-  })
   price: FlightPriceList;
-
-  @ManyToOne(() => AirportEntity, (airport) => airport.flightsFrom, {
-    onDelete: 'CASCADE',
-  })
-  from: AirportEntity;
-
-  @ManyToOne(() => AirportEntity, (airport) => airport.flightsTo, {
-    onDelete: 'CASCADE',
-  })
-  to: AirportEntity;
 }
